d78d5d593412ee9142f48d643fc02b3f0efc8933
Patrick Sheehan <psheehan89@gmail.com>
2017-04-20 10:53:43 -0700

    productOfAllOtherNumbers - Naive O(n^2) time O(n) space solution

--- productOfAllOtherNumbers.js
+++ productOfAllOtherNumbers.js
@@ -2,11 +2,25 @@
 // [1, 7, 4, 3]
 // [8, 12, 28, 21]
 
-function productOfAllOtherNumbers(array) {
+// Naive solution O(n^2) time O(n) space
+function productOfAllOtherNumbers(inputArray) {
   // Initialize a results array
+  var results = []
   // Iterate through the array
-  // Initialize a product variable
-  // Omit the current index
-  // Multiply the product variable by each element of the array
-  // Push the product into the results array
+  for (var i = 0; i < inputArray.length; i++) {
+    // Initialize a product variable
+    var product = 1;
+    for (var j = 0; j < inputArray.length; j++) {
+      // Omit the current index
+      if (i === j) continue;
+      // Multiply the product variable by each element of the array
+      product *= inputArray[j];
+    }
+    // Push the product into the results array
+    results.push(product)
+  }
+  console.log(results)
+  return results;
 }
+
+productOfAllOtherNumbers([1, 7, 4, 3])
