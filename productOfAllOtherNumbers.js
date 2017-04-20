7b7a0c644a3ffcfa0ad05784482ceb36fc1142d8
Patrick Sheehan <psheehan89@gmail.com>
2017-04-20 10:53:14 -0700

    productOfAllOtherNumbers - pseudocode skeleton

---
+++ productOfAllOtherNumbers.js
@@ -0,0 +1,12 @@
+
+// [1, 7, 4, 3]
+// [8, 12, 28, 21]
+
+function productOfAllOtherNumbers(array) {
+  // Initialize a results array
+  // Iterate through the array
+  // Initialize a product variable
+  // Omit the current index
+  // Multiply the product variable by each element of the array
+  // Push the product into the results array
+}
