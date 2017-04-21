5776c95dba1fd665a08129e7fcb5e1b2598050f2
Patrick Sheehan <psheehan89@gmail.com>
2017-04-20 18:40:19 -0700

    highestProductOfThree - pseudocode skeleton

---
+++ highestProductOfThree.js
@@ -0,0 +1,9 @@
+// Find the highest product of three numbers in an array
+
+function highestProductOfThree(inputArray) {
+  // Initialize a result variable
+  // Sort the input array
+  // Determine if the two most negative numbers are larger than the second and third largest numbers
+  // If so set the result to the product of the negative numbers * the largest positive number
+  // If not set the result to the product of the three largest numbers
+}
