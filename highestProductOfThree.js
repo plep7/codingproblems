f33bcfc94128cca7ea30afd0653b1aec769fb870
Patrick Sheehan <psheehan89@gmail.com>
2017-04-20 18:40:51 -0700

    highestProductOfThree implemented working solution

--- highestProductOfThree.js
+++ highestProductOfThree.js
@@ -1,9 +1,20 @@
 // Find the highest product of three numbers in an array
 
 function highestProductOfThree(inputArray) {
-  // Initialize a result variable
+  // Initialize a result variable to 1
+  var result = 1;
   // Sort the input array
+  inputArray.sort((a, b) => { return a - b; })
   // Determine if the two most negative numbers are larger than the second and third largest numbers
-  // If so set the result to the product of the negative numbers * the largest positive number
-  // If not set the result to the product of the three largest numbers
+  if (inputArray[0] * inputArray[1] > inputArray[inputArray.length - 2] * inputArray[inputArray.length - 3]) {
+    // If so set the result to the product of the negative numbers * the largest positive number
+    result = inputArray[0] * inputArray[1] * inputArray[inputArray.length - 1];
+  } else {
+    // If not set the result to the product of the three largest numbers
+    result = inputArray[inputArray.length - 1] * inputArray[inputArray.length - 2] * inputArray[inputArray.length - 3];
+  }
+  console.log(result)
+  return result;
 }
+
+highestProductOfThree([1, 2, -3, 0, 4, -5, -1])
