b8a35c73e6b2b3e1a4837ffda3ad1a0133d6505c
Patrick Sheehan <psheehan89@gmail.com>
2017-04-24 19:20:36 -0700

    rectangleIntersection - psuedocode skeleton

---
+++ rectangleIntersection.js
@@ -0,0 +1,25 @@
+function rectangleIntersection(r1, r2) {
+  // Find the smaller x and y coordinates
+  // Determine if there's BOTH x and y overlap
+  // If so, return the overlapping rectangle
+  // If not, return that there's no overlap
+
+}
+
+var rectangle1 = {
+  //  coordinates of bottom-left corner: 
+  'x': 1,
+  'y': 5,
+  //  width and height 
+  'width': 10, 
+  'height': 4
+}
+
+var rectangle2 = {
+  //  coordinates of bottom-left corner: 
+  'x': 6,
+  'y': 6,
+  //  width and height 
+  'width': 4, 
+  'height': 10
+}
