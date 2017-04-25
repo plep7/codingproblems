3e8f6ce03400d1a37b589d3e035781d4ee3d640b
Patrick Sheehan <psheehan89@gmail.com>
2017-04-24 19:40:09 -0700

    rectangleIntersection - complete solution

--- rectangleIntersection.js
+++ rectangleIntersection.js
@@ -1,9 +1,41 @@
 function rectangleIntersection(r1, r2) {
+  // Initialize a result
+  var result = {}, minX, minY, maxX, maxY;
   // Find the smaller x and y coordinates
-  // Determine if there's BOTH x and y overlap
-  // If so, return the overlapping rectangle
-  // If not, return that there's no overlap
+  if (r1.x < r2.x) {
+    minX = r1;
+    maxX = r2;
+  } else {
+    minX = r2;
+    maxX = r1;
+  }
+  if (r1.y < r2.y) {
+    minY = r1;
+    maxY = r2;
+  } else {
+    minY = r2;
+    maxY = r1;
+  }
 
+  console.log('minX', minX.x)
+  console.log('minY', minY.y)
+  console.log('maxX', maxX.x)
+  console.log('maxY', maxY.y)
+
+  // Determine if there's BOTH x and y overlap
+  if (minX.x + minX.width > maxX.x && minY.y + minY.height > maxY.y) {
+    // If so, build the overlapping rectangle
+    result.x = maxX.x;
+    result.y = maxY.y;
+    result.width = minX.x + minX.width - maxX.x;
+    result.height = minY.y + minY.height - maxY.y;
+  } else {
+    // If not, state that there's no overlap
+    result = 'There is no overlap between these rectangles'
+  }
+
+  console.log(result)
+  return result;
 }
 
 var rectangle1 = {
@@ -23,3 +55,5 @@ var rectangle2 = {
   'width': 4, 
   'height': 10
 }
+
+rectangleIntersection(rectangle1, rectangle2)
