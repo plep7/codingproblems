12b0483f24869da3cc4df7688e71a5134f2b551a
Patrick Sheehan <psheehan89@gmail.com>
2017-05-04 10:43:24 -0700

    palindromeRearranging.js - working solution

--- palindromeRearranging.js
+++ palindromeRearranging.js
@@ -7,14 +7,24 @@
 
 
 /*  The plan: Count the occurance of each letter in the string. As long as there's at most 1 odd
-    the string can be rearranged into a palindrom */
+    the string can be rearranged into a palindrome */
 
 function palindromeRearranging(inputString) {
   // Initialize countingObject and oddCounter
+  var counterObj = {}, oddCounter = 0;
   // Iterate through the string
-    // If letter exists, increment counter
-    // If not initialize key : value pair to letter : counter = 1
+  for (var i = 0; i < inputString.length; i++) {
+    // If letter exists, increment counter.  If not initialize key : value pair to letter : counter = 1
+    counterObj[inputString[i]] ? counterObj[inputString[i]]++ : counterObj[inputString[i]] = 1;
+  }
   // Iterate through the object
+  for (var key in counterObj) {
     // If the counter is odd, increment odd counter
+    if (counterObj[key] % 2 === 1) oddCounter++;
+  }
   // Return odd counter < 2
+  console.log(oddCounter < 2)
+  return oddCounter < 2;
 }
+
+palindromeRearranging("aabbbc")
