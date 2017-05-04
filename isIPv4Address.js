c5a954dd189ec4880b1e57b787015938b7ef0b2a
Patrick Sheehan <psheehan89@gmail.com>
2017-05-04 14:45:46 -0700

    isIPv4Address.js - pseudo code

--- isIPv4Address.js
+++ isIPv4Address.js
@@ -1,6 +1,9 @@
-// An IP address is a numerical label assigned to each device (e.g., computer, printer) participating in a computer network that uses the Internet Protocol for communication. There are two versions of the Internet protocol, and thus two versions of addresses. One of them is the IPv4 address.
+// An IP address is a numerical label assigned to each device (e.g., computer, printer) participating 
+// in a computer network that uses the Internet Protocol for communication. There are two versions of the 
+// Internet protocol, and thus two versions of addresses. One of them is the IPv4 address.
 
-// IPv4 addresses are represented in dot-decimal notation, which consists of four decimal numbers, each ranging from 0 to 255, separated by dots, e.g., 172.16.254.1.
+// IPv4 addresses are represented in dot-decimal notation, which consists of four decimal numbers, each 
+// ranging from 0 to 255, separated by dots, e.g., 172.16.254.1.
 
 // Given a string, find out if it satisfies the IPv4 address naming rules.
 
@@ -20,5 +23,8 @@
 // There is no first number.
 
 function isIPv4Address(inputString) {
-
+  // Split string into an array
+  // Return false if array length is less than 4
+  // Return false if any element is not a number
+  // Return false greater than 255 or less than 0
 }
