d4b9cce49958fb6707d33f78c32ed0a609ea19f0
Patrick Sheehan <psheehan89@gmail.com>
2017-05-04 10:33:15 -0700

    palindromeRearranging.js - problem statement

---
+++ palindromeRearranging.js
@@ -0,0 +1,10 @@
+// Example
+
+// For inputString = "aabb", the output should be
+// palindromeRearranging(inputString) = true.
+
+// We can rearrange "aabb" to make "abba", which is a palindrome.
+
+function palindromeRearranging(inputString) {
+
+}
