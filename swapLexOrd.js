59860112068c8e34783967527a8e49ea43f5a30d
Patrick Sheehan <psheehan89@gmail.com>
2017-05-08 23:24:51 -0700

    swapLexOrd.js - brainstorm and pseudo skeleton

--- swapLexOrd.js
+++ swapLexOrd.js
@@ -8,7 +8,15 @@
 // By swapping the given indices, you get the strings: "cbda", "cbad", "dbac", "dbca". The lexicographically largest string in this list is "dbca".
 
 function swapLexOrder(str, pairs) {
-
+  // Initialize a doesIt array, anyNewStr
+  // While we keep finding new strings
+  // Set anyNewStr to false
+  // Iterate through the pairs
+    // Swap the given indices
+    // Check if the new string is in the doesIt array
+    // If not, push the value into the array and set anyNewStr to true
+  // Sort the doesIt array largest to smallest
+  // Return the first element
 
 }
 
