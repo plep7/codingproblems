aadea917ddb6b11475a5b2adbcea86bf41b41111
Patrick Sheehan <psheehan89@gmail.com>
2017-05-18 15:51:29 -0700

    evenDigitsOnly.js - refactored using every method

--- evenDigitsOnly.js
+++ evenDigitsOnly.js
@@ -6,3 +6,12 @@
 // evenDigitsOnly(n) = true;
 // For n = 642386, the output should be
 // evenDigitsOnly(n) = false.
+
+function evenDigitsOnly(n) {
+  var numArray = n.toString().split('');
+
+  return numArray.every(element => element % 2 === 0)
+}
+
+console.log(evenDigitsOnly(248622))
+console.log(evenDigitsOnly(248621))
