73827e05a25e8be26cbe1666c72b07321d731374
Patrick Sheehan <psheehan89@gmail.com>
2017-05-18 12:13:39 -0700

    minesweeper.js - check if adjacent spaces exists

--- minesweeper.js
+++ minesweeper.js
@@ -15,9 +15,34 @@
 
 function minesweeper(matrix) {
   // Initialize a counter for each square to 0
-  var result = new Array(matrix.length).fill(new Array(matrix.length).fill(0))
-  console.log(result)
+  var results, adj;
+
+  results = new Array(matrix.length).fill(new Array(matrix.length).fill(0));
+  console.log(matrix)
+  // Visit each square
+  for (var i = 0; i < matrix.length; i++) {
+    for (var j = 0; j < matrix[i].length; j++) {
+      // Reset presence check
+      adj = { 
+          pRow: false,
+          pCol: false,
+          nRow: false, 
+          nCol: false
+        };
+
+      // Check if an adjacent space even exists
+      if (matrix[i - 1] !== undefined) adj.pRow = true;
+      if (matrix[i + 1] !== undefined) adj.nRow = true;
+      if (matrix[i][j - 1] !== undefined) adj.pCol = true;
+      if (matrix[i][j + 1] !== undefined) adj.nCol = true;
+
+      console.log(i, j, adj)
+    }
+  }
   // Check the adjacent squares for mines
+    // Check if there is a previous row
+      // If so check if there is a previous column
+        // If so check
   // If there is a mine in an adjacent square, increment counter
 
 }
