c9b1798f73dcdb5196b0afe6f5c009aa22f79fa9
Patrick Sheehan <psheehan89@gmail.com>
2017-05-18 11:41:20 -0700

    minesweeper.js - problem statement

---
+++ minesweeper.js
@@ -0,0 +1,18 @@
+// create a Minesweeper game setup.
+
+// Example
+
+// For
+
+// matrix = [[true, false, false],
+//           [false, true, false],
+//           [false, false, false]]
+// the output should be
+
+// minesweeper(matrix) = [[1, 2, 1],
+//                        [2, 1, 1],
+//                        [1, 1, 1]]       
+
+function minesweeper(matrix) {
+
+}
