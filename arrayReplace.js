e2d5ef1263fd12525abd2bd7efd02d5703e23eeb
Patrick Sheehan <psheehan89@gmail.com>
2017-05-18 15:39:02 -0700

    arrayReplace.js - working solution

--- arrayReplace.js
+++ arrayReplace.js
@@ -6,5 +6,13 @@
 // arrayReplace(inputArray, elemToReplace, substitutionElem) = [3, 2, 3]
 
 function arrayReplace(inputArray, elemToReplace, substitutionElem) {
-
+  // If the element matches the elemToReplace, substitute it
+  for (var i = 0; i < inputArray.length; i++) {
+    if (inputArray[i] === elemToReplace) inputArray[i] = substitutionElem;
+  }
+  return inputArray;
 }
+
+var inputArray = [1, 2, 1]
+
+arrayReplace(inputArray, 1, 3)
