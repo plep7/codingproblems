63991b5e4be9f1073ed52f0a022b84d636260c0a
Patrick Sheehan <psheehan89@gmail.com>
2017-05-19 13:12:01 -0700

    variableName.js - pseudocode skeleton

--- variableName.js
+++ variableName.js
@@ -10,3 +10,12 @@
 // variableName(name) = false;
 // For name = "2w2", the output should be
 // variableName(name) = false.
+
+function variableName(name) {
+  // Check if the first element is a number
+    // If so return false
+
+  // Use a regex to determine if the string only contains A-Z 0-9 and _
+  // If so return true, if not return false
+
+}
