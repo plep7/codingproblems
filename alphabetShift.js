7bf2b56f193e10582c3fe227a22ce5fbb83360ab
Patrick Sheehan <psheehan89@gmail.com>
2017-05-19 13:38:24 -0700

    alphabetString.js - added some variables to be more explicit. Workign solution

--- alphabetShift.js
+++ alphabetShift.js
@@ -6,7 +6,17 @@
 // alphabeticShift(inputString) = "dsbaz".
 
 function alphabeticShift(inputString) {
+  // Initialize outputString and letter
+  var outputString = '', letterCode, nextLetter;
   // Iterate through the string
+  for (var i = 0; i < inputString.length; i++) {
+    letterCode = inputString.charCodeAt(i);
     // Increment each charCodes
+    nextLetter = String.fromCharCode(letterCode + 1)
+    inputString[i] === 'z' ? outputString += 'a' : outputString += nextLetter;
+  }
     // If the letter is Z change it to A
+  return outputString
 }
+
+console.log(alphabeticShift('crazy'))
