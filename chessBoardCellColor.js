c40c088a42f58f0a75fb6005b2e7ffcb725bb6d1
Patrick Sheehan <psheehan89@gmail.com>
2017-05-23 11:35:07 -0700

    chessBoardCellColor.js - refactored to sleek ES notation

--- chessBoardCellColor.js
+++ chessBoardCellColor.js
@@ -5,13 +5,8 @@
 // For cell1 = "A1" and cell2 = "C3", the output should be
 // chessBoardCellColor(cell1, cell2) = true.
 
-function chessBoardCellColor(cell1, cell2) {
-  return cellValue(cell1) % 2 === cellValue(cell2) % 2;
+var chessBoardCellColor = (cell1, cell2) => cellValue(cell1) % 2 === cellValue(cell2) % 2;
 
-}
-
-function cellValue(cell) {
-  return cell.charCodeAt(0) + Number(cell[1]);
-}
+var cellValue = cell => cell.charCodeAt(0) + Number(cell[1]);
 
 console.log(chessBoardCellColor('B3', 'A4'))
