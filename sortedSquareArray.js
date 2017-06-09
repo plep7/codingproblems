a3074599986970726aa4dbdd0bdcd0047c82beb4
Patrick Sheehan <psheehan89@gmail.com>
2017-06-09 13:15:17 -0700

    sortedSquareArray.js naive solution

--- sortedSquareArray.js
+++ sortedSquareArray.js
@@ -9,7 +9,15 @@
 
 // The array of squared integers from array is: [36, 16, 1, 4, 9, 25]. When sorted, it becomes: [1, 4, 9, 16, 25, 36].
 
+
+var array = [-6, -4, 1, 2, 3, 5];
+
 function sortedSquaredArray(array) {
+  var results = []
   // Iterate through the array and square each value
+  array.forEach(element => results.push(element * element))
   // Return the sorted squared array
+  return results.sort((a, b) => a - b)
 }
+
+console.log(sortedSquaredArray(array))
