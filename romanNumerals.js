509bbc812f9842c270608d445c9e7171e3206390
Patrick Sheehan <psheehan89@gmail.com>
2017-06-09 09:36:42 -0700

    romanNumeral.js - create a function that converts to roman numerals and vice versa

---
+++ romanNumerals.js
@@ -0,0 +1,14 @@
+var RomanNumeral = {
+  toRoman(number) {
+    console.log(number)
+  },
+  fromRoman(numeral) {
+    console.log(numeral)
+  }
+}
+
+
+var conversion
+
+RomanNumeral.toRoman(1000)
+RomanNumeral.fromRoman('M')
