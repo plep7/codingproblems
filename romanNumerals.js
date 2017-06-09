47889967bbfae01cfccf6bb7278dc48955b44e21
Patrick Sheehan <psheehan89@gmail.com>
2017-06-09 09:54:11 -0700

    romanNumeral.js - pseudocode to convert from roman to number

--- romanNumerals.js
+++ romanNumerals.js
@@ -4,11 +4,22 @@ var RomanNumeral = {
   },
   fromRoman(numeral) {
     console.log(numeral)
+    // initialize a result variable to 0
+    // iterate through the string
+    // if the next value is smaller, subtract from total
+    // else add it to the total
   }
 }
 
 
-var conversion
+var conversion = {
+  I: 1,
+  V: 5,
+  X: 10,
+  L: 50,
+  C: 100,
+  M: 1000
+}
 
 RomanNumeral.toRoman(1000)
 RomanNumeral.fromRoman('M')
